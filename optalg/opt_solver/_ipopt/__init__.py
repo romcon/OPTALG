19cd51860c5a3a20032da0b990bb08b7b5631a1a
TamerSobhy <tamersobhy.eg@gmail.com>
2023-08-28 09:18:47 -0400

    Import dll files with winmode=0 to avoid dll importing error. Modify build bat file to be able to download ipopt solver

--- optalg/opt_solver/_ipopt/__init__.py
+++ optalg/opt_solver/_ipopt/__init__.py
@@ -6,5 +6,12 @@
 # OPTALG is released under the BSD 2-clause license. #
 #****************************************************#
 
+import sys
+if sys.platform[:3].lower() == "win":
+    import ctypes as cts
+    import os
+    cts.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "IpOptFSS.dll"), winmode=0)
+    cts.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "IpOpt-vc10.dll"), winmode=0)
+
 from .cipopt import *
 
